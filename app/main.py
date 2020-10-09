0c01718e18debfb6828629141a9e9be9e9b64b20
Phuc Pham <ppham@PP-EN-C02C42RAM6M.local>
2020-10-08 18:01:41 -0700

    try with index.html for launch

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
   
 @app.route("/") 
 def home_view(): 
-    return "<h1>Welcome to Geeks for Geeks</h1>"
+    return render_template("index.html")
 
 @app.route("/launch")
 def launch():
